073f2b04206816147b823de5eb91e4fe95a0441c
lesenelir <miaomiaobabyzy@gmail.com>
2022-05-08 13:15:23 +0800

    update

--- src/03-闭包/06-闭包应用-YE.js
+++ src/03-闭包/06-闭包应用-YE.js
@@ -25,13 +25,23 @@ function classroom(name) {
       console.log(stuArr)
     },
     delete(name) {
-      stuArr.filter((item) => {
+      let res = stuArr.filter((item) => {
         return item !== name
       })
+      console.log(res)
     }
   }
 
-  return obj // 返回一个对象
+  return obj // 返回一个对象 形成闭包
 }
 
+// 通过用函数去封装数据、操作数据的方法，返回给外界使用
+
+const student1 = classroom()
+student1.add('l1')
+student1.add('l2')
+student1.add('l3')
+student1.delete('l1')
+
+// Note： 闭包的应用场景： 防抖、设置私有变量、单例模式
 
